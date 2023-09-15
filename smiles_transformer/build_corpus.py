5070bc90e6b10173b9a120e9500e6da7ed00366b
Maria Eremeeva <107473507+m19eremeeva@users.noreply.github.com>
2023-09-15 23:54:41 +0300

    Update build_corpus.py

--- smiles_transformer/build_corpus.py
+++ smiles_transformer/build_corpus.py
@@ -6,8 +6,8 @@ from utils import split
 
 def main():
     parser = argparse.ArgumentParser(description='Build a corpus file')
-    parser.add_argument('--in_path', '-i', type=str, default='data/chembl24_bert_train.csv', help='input file')
-    parser.add_argument('--out_path', '-o', type=str, default='data/chembl24_corpus.txt', help='output file')
+    parser.add_argument('--in_path', '-i', type=str, default='/content/chembl24_bert_train.csv', help='input file')
+    parser.add_argument('--out_path', '-o', type=str, default='/content/chembl24_corpus.txt', help='output file')
     args = parser.parse_args()
 
     smiles = pd.read_csv(args.in_path)['first'].values
